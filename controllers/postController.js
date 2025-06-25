0a77e89436ff14b3996e9262b6745447ad47e450
DemiusLs <dloss320@gmail.com>
2025-06-25 18:03:10 +0200

    feat: bonus to fix

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,13 +12,11 @@ const index = (req, res) => {
 
 const show = (req, res) => {
 
-    const sql = 'SELECT * FROM posts WHERE id = ?';
-    const id = req.params.id
-
-    connection.query(sql, [id], (err, results) => {
+    const sql = "SELECT	* INNER JOIN `post_tag` ON   `posts`.`id` = `post_tag`.`post_id` INNER JOIN `tags` ON  `tags`.`id` = `post_tag`.`tag_id` WHERE `posts`.`id` =1  const id = req.params.id "
+      connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
-        res.json(results[0]);
+        res.json(results);
     });
 }
 
