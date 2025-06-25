02ef9d4248007e1b1f0c4d87ef54a75de079f3de
DemiusLs <dloss320@gmail.com>
2025-06-25 17:29:30 +0200

    feat:milestone 3 done

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,10 +12,13 @@ const index = (req, res) => {
 
 const show = (req, res) => {
 
-    const id = req.params.id
-    res.json(
-        `risposta da show a ${id}`
-    )
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+        res.json(results[0]);
+    });
 }
 
 const store = (req, res) => {
@@ -36,10 +39,12 @@ const update = (req, res) => {
 
 const destroy = (req, res) => {
 
-    const id = req.params.id
-    res.json(
-        `risposta da destroy a ${id}`
-    )
+    const sql = 'DELETE * FROM posts WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.sendStatus(204);
+    });
 
 }
 
